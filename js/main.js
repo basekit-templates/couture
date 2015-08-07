e04d6f0050326fa486b96f07ac93b0cbc1db9a6b
Ivan Pik <ivan.pik@basekit.com>
2015-08-07 12:09:33 +0100

    Feature JS fadeout effect update

--- js/main.js
+++ js/main.js
@@ -15,27 +15,61 @@ $( ".navigation-item.folder" ).each(function() {
 
 // Set a data attribute that matches the content of the nav item
 var link = document.querySelectorAll('.item-name');
-for (i = 0; i < link.length; i++) { 
+for (i = 0; i < link.length; i++) {
     link[i].setAttribute("data-hover", link[i].innerHTML);
 }
+
+
+
+
+
+
+var featureHeight = $(".feature-container").height();
+var featurePosition = $(".feature-container").position();
+var featureTriggerValue = featureHeight + featurePosition.top;
+var window_height = window.innerHeight;
+var offsetAmount;
+
+function checkFeatureDimensions() {
+    featureHeight = $(".feature-container").height();
+    featurePosition = $(".feature-container").position();
+    featureTriggerValue = featureHeight + featurePosition.top;
+    window_height = window.innerHeight;
+}
+
+
 
 
 // Feature parralax fade effect
 var scrollEffect = function (elem) {
 
     // How high the window is
-    var window_height = window.innerHeight;
 
-    // Apply styles as user scrolls
-    elem.setAttribute(
-        "style", "opacity:" + Math.max(0, parseFloat(1-(1/window_height*window.pageYOffset)).toFixed(2)) + "; -webkit-transform: translateY(" + window.pageYOffset + "px)"
-    );
+    if ( window.pageYOffset > (featureTriggerValue - window_height)  ) {
+
+        if(featureTriggerValue > window_height) {
+            offsetAmount = window.pageYOffset - (featureTriggerValue - window_height);
+        }
+        else {
+            offsetAmount = window.pageYOffset;
+        }
+
+        // Apply styles as user scrolls
+        elem.setAttribute(
+            "style", "opacity:" + Math.max(0, parseFloat(1-(2/window_height*offsetAmount)).toFixed(2)) + "; -webkit-transform: translateY(" + offsetAmount + "px)"
+        );
+     }
 
 };
 
 var elem = document.querySelector('.content-wrap');
 
+
 window.onscroll = function(e) {
     scrollEffect(elem);
 };
+
+$( window ).resize(function() {
+  checkFeatureDimensions();
+});
 
